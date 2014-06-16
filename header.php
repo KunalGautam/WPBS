8981b02d4d3ced089f25442a4d58280e1a01923b
Kunal Gautam <kunal@KalpanaPC.(none)>
2014-06-16 22:16:59 +0530

    Modified Theme details. Added Bootstrap 3.1.1 files.

--- header.php
+++ header.php
@@ -8,6 +8,15 @@
 	<title><?php bloginfo( 'name' ); ?> <?php bloginfo( 'description' ); ?></title>
 
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css"/>
+<!-- Bootstrap -->
+    <link href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/html5shiv.js"></script>
+      <script src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/respond.min.js"></script>
+    <![endif]-->
 
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 

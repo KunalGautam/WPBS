d2de674b5fb03a3380fd70740151f935f1285f35
Kunal Gautam <kunal@ikunal.in>
2015-03-31 21:59:08 +0530

    Single Page display modified.

--- index.php
+++ index.php
@@ -1,23 +1,15 @@
 <?php get_header(); ?>
 
 <?php if(have_posts()) : ?>
-
 	<?php while(have_posts()) : the_post(); ?>
-	
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> <!-- http://codex.wordpress.org/Template_Tags/post_class -->
+ 
+		<div class="row">
+
 
  			<header>  
 
-				<h3>
-				<a href="<?php the_permalink(); ?>">
-				<?php the_title(); ?>
-				</a>
-				</h3> <!-- Title of the post -->
+				<h3><?php the_title(); ?></h3> <!-- Title of the post -->
 	
-				<?php echo get_the_date( ); ?> <!-- Date published -->
-				<?php the_time(); ?>  <!-- Time published -->
-				<?php  the_author(); ?> <!-- Author of the post -->
-
 			</header>	
 
 			<?php 	
@@ -27,28 +19,30 @@
 				</aside>
 			<?php } ?>
 
+
 			<?php the_content(); ?> <!-- Contents of a post -->
 
 			<footer>	
-
+				
 				<?php the_category(', ') ?>  <!-- Post category -->
 				<?php the_tags(', ') ?>  <!-- Post tags -->
 	
-				 <?php comments_popup_link(); ?>.
+				 <?php comments_popup_link(); ?>. <!-- comments_number -->
 				<?php edit_post_link(); ?>
+				
+				<?php comments_template(); ?>
 
 
 			</footer>
 
-		
 				
-		</article>
+		</div>
 
 
+	
 	<?php endwhile; ?>
- 
- 
+
+
 <?php endif; ?>
    
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
